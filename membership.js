30f6f805b6cf6b830944e1d92ab904b1bf5f0467
Mohammed Ali <binbasri.india@gmail.com>
2024-02-25 13:09:29 +0530

    added the form feilds

--- membership.js
+++ membership.js
@@ -1,54 +1,65 @@
 
 //billing and shading image(product) when selected
-var count=0;
+var count = 0;
 var totalPrice = 1370;
+var selectedImages = {
+    "selectedImage1": false,
+    "selectedImage2": false,
+    "selectedImage3": false,
+    "selectedImage4": false,
+    "selectedImage5": false,
+    "selectedImage6": false
+};
 
 function changeShade1() {
     var image = document.getElementById('selectedImage1');
     image.classList.toggle('selected');
     var productPrice = 392;
-   
-    if(image.classList.contains('selected'))
-    {
+
+    if (image.classList.contains('selected')) {
         totalPrice += productPrice;
-        
-    }  else
-    {   
+        count++;
+        selectedImages["selectedImage1"] = true;
+
+    } else {
         totalPrice -= productPrice;
+        count--;
+        selectedImages["selectedImage1"] = false;
     }
 
     updatePriceBox(totalPrice);
 }
+
 function changeShade2() {
     var image = document.getElementById('selectedImage2');
     image.classList.toggle('selected');
 
     var productPrice = 49;
-   
-    if(image.classList.contains('selected'))
-    {
+
+    if (image.classList.contains('selected')) {
         totalPrice += productPrice;
         count++;
-    }  else
-    {   
+        selectedImages["selectedImage2"] = true;
+    } else {
         count--;
         totalPrice -= productPrice;
+        selectedImages["selectedImage2"] = false;
     }
     updatePriceBox(totalPrice);
 }
 function changeShade3() {
     var image = document.getElementById('selectedImage3');
-    image.classList.toggle('selected'); 
+    image.classList.toggle('selected');
     var productPrice = 49;
-   
-    if(image.classList.contains('selected'))
-    {
+
+    if (image.classList.contains('selected')) {
         totalPrice += productPrice;
         count++;
-    }  else
-    {   
+        selectedImages["selectedImage3"] = true;
+    } else {
         count--;
         totalPrice -= productPrice;
+        selectedImages["selectedImage3"] = false;
     }
 
     updatePriceBox(totalPrice);
@@ -56,16 +67,16 @@ function changeShade3() {
 function changeShade4() {
     var image = document.getElementById('selectedImage4');
     image.classList.toggle('selected');
-     var productPrice = 234;
-   
-    if(image.classList.contains('selected'))
-    {
+    var productPrice = 234;
+
+    if (image.classList.contains('selected')) {
         totalPrice += productPrice;
         count++;
-    }  else
-    {   
+        selectedImages["selectedImage4"] = true;
+    } else {
         count--;
         totalPrice -= productPrice;
+        selectedImages["selectedImage4"] = false;
     }
 
     updatePriceBox(totalPrice);
@@ -74,15 +85,15 @@ function changeShade5() {
     var image = document.getElementById('selectedImage5');
     image.classList.toggle('selected');
     var productPrice = 490;
-   
-    if(image.classList.contains('selected'))
-    {
+
+    if (image.classList.contains('selected')) {
         totalPrice += productPrice;
         count++;
-    }  else
-    {   
+        selectedImages["selectedImage5"] = true;
+    } else {
         count--;
         totalPrice -= productPrice;
+        selectedImages["selectedImage5"] = false;
     }
 
     updatePriceBox(totalPrice);
@@ -93,15 +104,15 @@ function changeShade6() {
 
 
     var productPrice = 49;
-   
-    if(image.classList.contains('selected'))
-    {
+
+    if (image.classList.contains('selected')) {
         totalPrice += productPrice;
         count++;
-    }  else
-    {   
+        selectedImages["selectedImage6"] = true;
+    } else {
         count--;
         totalPrice -= productPrice;
+        selectedImages["selectedImage6"] = false;
     }
 
     updatePriceBox(totalPrice);
@@ -109,89 +120,82 @@ function changeShade6() {
 
 
 
-function toggleInfo1() 
-{
-var infoBox = document.getElementById('infoBox1');
-infoBox.classList.toggle('show');
+function toggleInfo1() {
+    var infoBox = document.getElementById('infoBox1');
+    infoBox.classList.toggle('show');
 }
-function toggleInfo2() 
-{
-var infoBox = document.getElementById('infoBox2');
-infoBox.classList.toggle('show');
+function toggleInfo2() {
+    var infoBox = document.getElementById('infoBox2');
+    infoBox.classList.toggle('show');
 }
-function toggleInfo3() 
-{
-var infoBox = document.getElementById('infoBox3');
-infoBox.classList.toggle('show');
+function toggleInfo3() {
+    var infoBox = document.getElementById('infoBox3');
+    infoBox.classList.toggle('show');
 }
-function toggleInfo4() 
-{
-var infoBox = document.getElementById('infoBox4');
-infoBox.classList.toggle('show');
+function toggleInfo4() {
+    var infoBox = document.getElementById('infoBox4');
+    infoBox.classList.toggle('show');
 }
-function toggleInfo5() 
-{
-var infoBox = document.getElementById('infoBox5');
-infoBox.classList.toggle('show');
+function toggleInfo5() {
+    var infoBox = document.getElementById('infoBox5');
+    infoBox.classList.toggle('show');
 }
-function toggleInfo6() 
-{
-var infoBox = document.getElementById('infoBox6');
-infoBox.classList.toggle('show');
+function toggleInfo6() {
+    var infoBox = document.getElementById('infoBox6');
+    infoBox.classList.toggle('show');
 }
 
 // Close info box if clicked outside of it
 
 document.addEventListener('click', function (event) {
-var infoBox = document.getElementById('infoBox1');
-if (!event.target.matches('img') && !event.target.closest('.info-container')) {
-    infoBox.classList.remove('show');
-}
+    var infoBox = document.getElementById('infoBox1');
+    if (!event.target.matches('img') && !event.target.closest('.info-container')) {
+        infoBox.classList.remove('show');
+    }
 });
 document.addEventListener('click', function (event) {
-var infoBox = document.getElementById('infoBox2');
-if (!event.target.matches('img') && !event.target.closest('.info-container')) {
-    infoBox.classList.remove('show');
-}
+    var infoBox = document.getElementById('infoBox2');
+    if (!event.target.matches('img') && !event.target.closest('.info-container')) {
+        infoBox.classList.remove('show');
+    }
 });
 document.addEventListener('click', function (event) {
-var infoBox = document.getElementById('infoBox3');
-if (!event.target.matches('img') && !event.target.closest('.info-container')) {
-    infoBox.classList.remove('show');
-}
+    var infoBox = document.getElementById('infoBox3');
+    if (!event.target.matches('img') && !event.target.closest('.info-container')) {
+        infoBox.classList.remove('show');
+    }
 });
 document.addEventListener('click', function (event) {
-var infoBox = document.getElementById('infoBox4');
-if (!event.target.matches('img') && !event.target.closest('.info-container')) {
-    infoBox.classList.remove('show');
-}
+    var infoBox = document.getElementById('infoBox4');
+    if (!event.target.matches('img') && !event.target.closest('.info-container')) {
+        infoBox.classList.remove('show');
+    }
 });
 document.addEventListener('click', function (event) {
-var infoBox = document.getElementById('infoBox5');
-if (!event.target.matches('img') && !event.target.closest('.info-container')) {
-    infoBox.classList.remove('show');
-}
+    var infoBox = document.getElementById('infoBox5');
+    if (!event.target.matches('img') && !event.target.closest('.info-container')) {
+        infoBox.classList.remove('show');
+    }
 });
 document.addEventListener('click', function (event) {
-var infoBox = document.getElementById('infoBox6');
-if (!event.target.matches('img') && !event.target.closest('.info-container')) {
-    infoBox.classList.remove('show');
-}
+    var infoBox = document.getElementById('infoBox6');
+    if (!event.target.matches('img') && !event.target.closest('.info-container')) {
+        infoBox.classList.remove('show');
+    }
 });
 
 
 function calculateTotalPrice(totalPrice) {
-    var discount =0;
-    if(count>=2)
-    discount=0.05;
-    if(discount==0.05)
-   {
+    var discount = 0;
+    if (count >= 2)
+        discount = 0.05;
+    if (discount == 0.05) {
         totalPrice = totalPrice - (totalPrice * discount);
         totalPrice = Math.ceil(totalPrice);
         totalPrice = Math.max(totalPrice, 1370); // Ensure totalPrice doesn't go below zero
-   }
-   var disc = document.getElementById('disc');
-   disc.textContent = 'Total Price: INR. ' + totalPrice.toFixed(2);
+    }
+    var disc = document.getElementById('disc');
+    disc.textContent = 'Total Price: INR. ' + totalPrice.toFixed(2);
 }
 
 function updatePriceBox(totalPrice) {
@@ -200,28 +204,41 @@ function updatePriceBox(totalPrice) {
 }
 
 
-
-
-
-
 // Google Forms Submit
 function executeGoogleFormsSubmit() {
-    // Build the Field Ids and Answers dictionary object
-    // (replace with your Google Form Ids and Answers)
-    var bodyValues = {
-        "entry.306042534": document.getElementById("name").value,
-        "entry.193107409": document.getElementById("emial").value,
-        "entry.193107409": document.getElementById("dob").value,
-        "entry.193107409": document.getElementById("year").value,
-        "entry.193107409": document.getElementById("phone").value,
-        "entry.193107409": document.getElementById("branch").value,
-        "entry.193107409": document.getElementById("gender").value,
-        "entry.193107409": document.getElementById("utr").value,
+    var formFields = {
+        "entry.1884265043": "name",
+        "entry.332621004": "email",
+        "entry.117351985": "year",
+        "entry.1619078457": "phone",
+        "entry.1212348438": "branch",
+        "entry.754680598": "gender",
+        "entry.1770329536": "utr",
+        "entry.635334495_year": "dob_year",
+        "entry.635334495_month": "dob_month",
+        "entry.635334495_day": "dob_day"
     };
 
-    // check if there is any empty field
-    for (var key in bodyValues) {
-        if (bodyValues[key] === "") {
+    var formData = new FormData();
+    var dob = document.getElementById("dob").value.split("-");
+
+    // Fill in the form data
+    for (var entryId in formFields) {
+        var fieldId = formFields[entryId];
+        var value = (fieldId.startsWith("dob_")) ? dob[fieldId.split("_")[1] === "year" ? 0 : fieldId.split("_")[1] === "month" ? 1 : 2] : document.getElementById(fieldId).value;
+        formData.append(entryId, value);
+    }
+
+    const chapters = ["CS", "SPS", "ITS", "CIS", "RAS", "PES"];
+    for (var i = 0; i < chapters.length; i++) {
+        if (selectedImages["selectedImage" + i]) {
+            formData.append("entry.1729285030", chapters[i]);
+        }
+    }
+
+    // Check for empty fields
+    for (var value of formData.values()) {
+        if (value === "") {
             alert("Please fill in all the fields");
             return;
         }
@@ -231,35 +248,23 @@ function executeGoogleFormsSubmit() {
     var xhr = new XMLHttpRequest();
 
     // Set the request method and URL
-    xhr.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSc9j9rrJspdVSeb0XsEEY8XRwKqdi2WwJu1vOZ-NDGlXQhQHA/viewform?usp=sf_link");
-
-    // Set the request headers
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    // Convert the bodyValues object to a URL-encoded string
-    var formData = Object.keys(bodyValues).map(key => encodeURIComponent(key) + "=" + encodeURIComponent(bodyValues[key])).join("&");
+    xhr.open("POST", "https://docs.google.com/forms/u/0/d/e/1FAIpQLSc9j9rrJspdVSeb0XsEEY8XRwKqdi2WwJu1vOZ-NDGlXQhQHA/formResponse");
 
     // Set the onload event handler
     xhr.onload = function () {
         // Use the response status and text
         console.log(`Status : ${xhr.status} ${xhr.statusText}`);
         console.log(`Body : \n${xhr.responseText}`);
-
     };
 
     // Send the POST request with the form data
-    xhr.send(formData);
+    xhr.send(new URLSearchParams(formData).toString());
 
     // Reset the form
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
-    document.getElementById("dob").value = "";
     document.getElementById("year").value = "";
     document.getElementById("phone").value = "";
     document.getElementById("branch").value = "";
-    document.getElementById("gender").value = "";
-    document.getElementById("utr").value = "";
-
     alert("Your response has been submitted successfully");
-
 }
